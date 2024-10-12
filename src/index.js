4522ae7de0629186e3e9c668cf383f4ebdeb90fb
FavelLooking <intwestpol@gmail.com>
2024-10-12 14:12:14 +0400

    feat: parse username from argv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello, Node.js!");
+import { promises as fs } from "fs";
+import path from "path";
+import * as url from "url";
+
+const parseArg = () => {
+  const arg = process.argv[3];
+  const trimmedArg = arg.replace("--", "");
+  let equalIndex = trimmedArg.indexOf("=");
+  return trimmedArg.slice(equalIndex + 1);
+};
+
+const initializeProgram = () => {
+  let userName = parseArg();
+  process.stdout.write(`Welcome to the File Manager, ${userName}!`);
+};
+
+initializeProgram();
