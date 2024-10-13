1360ff5c21f4b9940f7bd47ffd0ac711b29da34b
FavelLooking <intwestpol@gmail.com>
2024-10-13 23:50:53 +0400

    feat: add error handling for createFile function

--- src/createFile.js
+++ src/createFile.js
@@ -1,8 +1,25 @@
 import * as path from "path";
-import fs from "fs";
+import { promises as fs } from "fs";
+import { handleInvalidData } from "./index.js";
 
-export const createFile = (currentDir, targetFile) => {
-  const filePath = path.join(currentDir, targetFile[0]);
-  const writeStream = fs.createWriteStream(filePath);
-  writeStream.end();
+export const createFile = async (currentDir, args) => {
+  if (args.length !== 1) {
+    handleInvalidData();
+    return;
+  }
+
+  const filePath = path.join(currentDir, args[0]);
+  try {
+    await fs.access(filePath);
+    console.log(`Operation failed`);
+    console.log(`File already exists: ${filePath}`);
+    return;
+  } catch (err) {
+    try {
+      await fs.writeFile(filePath, "", { flag: "wx" });
+      console.log(`File created: ${filePath}`);
+    } catch (error) {
+      console.log(`Operation failed`);
+    }
+  }
 };
