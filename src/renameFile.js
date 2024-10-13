2a302079f170838915068be1d1e1a020d4936ae5
FavelLooking <intwestpol@gmail.com>
2024-10-14 00:00:09 +0400

    feat: add error handling for rename function

--- src/renameFile.js
+++ src/renameFile.js
@@ -1,10 +1,26 @@
 import * as path from "path";
 import { promises as fs } from "fs";
+import { handleInvalidData } from "./index.js";
 
 export const renameFile = async (currentDir, args) => {
+  if (args.length !== 2) {
+    handleInvalidData();
+    return;
+  }
   const [oldFileName, newFileName] = args;
-  const filePath = path.join(currentDir, oldFileName);
-  const newFilePath = path.join(currentDir, newFileName);
+  const oldFilePath = path.isAbsolute(oldFileName)
+    ? oldFileName
+    : path.join(currentDir, oldFileName);
 
-  await fs.rename(filePath, newFilePath);
+  const newFilePath = path.isAbsolute(newFileName)
+    ? newFileName
+    : path.join(currentDir, newFileName);
+
+  try {
+    await fs.access(oldFilePath);
+    await fs.rename(oldFilePath, newFilePath);
+    console.log(`File was renamed`);
+  } catch (error) {
+    console.log("Operation failed");
+  }
 };
