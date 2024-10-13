a141641b34d102610902434f1ed1efa0aeb5d2c8
FavelLooking <intwestpol@gmail.com>
2024-10-13 16:05:48 +0400

    feat: add copy file feature

--- src/copyFile.js
+++ src/copyFile.js
@@ -1,30 +1,28 @@
 import * as path from "path";
-import { stdout as output } from "node:process";
-import { promises as fs } from "fs";
+import * as fs from "fs";
 
-// const makeFolderPath = (path) => {
-//   console.log(path);
-//   const indexSeparator = path.lastIndexOf("/" || "\\");
-//   console.log(`indexSeparator: ${indexSeparator}`);
-//   const folderPath = path.slice(indexSeparator);
-//   console.log(`folderPath: ${folderPath}`);
-//   return folderPath;
-// };
 export const copyFile = async (currentDir, args) => {
   const [pathToFile, newDirectoryPath] = args;
   const folderPath = path.dirname(newDirectoryPath);
-  console.log(folderPath);
-  // console.log(pathToFile);
-  // console.log(folderPath);
-  // const readStream = fs.createReadStream(pathToFile);
-  // const writeStream = fs.createWriteStream(newDirectoryPath);
-  // readStream.pipe(writeStream);
 
   try {
-    await fs.mkdir(folderPath, { recursive: true });
+    await fs.promises.mkdir(folderPath, { recursive: true });
     const readStream = fs.createReadStream(pathToFile);
     const writeStream = fs.createWriteStream(newDirectoryPath);
-    writeStream.end();
+
+    readStream.pipe(writeStream);
+
+    writeStream.on("finish", () => {
+      console.log("File copied successfully!");
+    });
+    writeStream.on("error", (err) => {
+      console.error("Error writing file:", err);
+      readStream.destroy();
+    });
+    readStream.on("error", (err) => {
+      console.error("Error reading file:", err);
+      writeStream.end();
+    });
   } catch (err) {
     console.error(err);
   }
